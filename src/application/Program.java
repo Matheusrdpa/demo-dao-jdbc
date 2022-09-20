8838a4d4243c8d81530c24b009455f66885a269e
Matheus Ribeiro <Nogahzera@gmail.com>
2022-09-20 12:00:07 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import entities.Department;
 import entities.Seller;
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 
 import java.util.Date;
 
@@ -9,6 +11,7 @@ public class Program {
     public static void main(String[] args){
         Department obj = new Department(1,"Books");
         Seller seller = new Seller(21,"Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+        SellerDao sellerDao = DaoFactory.createSellerDao();
         System.out.println(seller);
     }
 }
